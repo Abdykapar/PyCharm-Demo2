d5381cb64ae465121a66d76e05ffd57887e98052
= <=>
2017-04-30 15:06:18 +0600

    Migrations

--- App/models.py
+++ App/models.py
@@ -9,6 +9,8 @@ class Bolumder(models.Model):
     NameKir = models.CharField(max_length=200)
     Kod = models.IntegerField(null=True, blank=True)
     NumTandoo = models.IntegerField(default=0)
+    Type = models.CharField(max_length=100,default='')
+    Orun = models.IntegerField(default=0)
 
     def __unicode__(self):
         return self.NameTur
@@ -96,3 +98,10 @@ class MyTable(models.Model):
 class RayonExp(models.Model):
     Name = models.CharField(max_length=200)
     ExpName = models.ForeignKey(LkpRayon)
+
+
+class iv(models.Model):
+    name = models.ForeignKey(Bolumder)
+    bolum_pop = models.IntegerField(default=0)
+    bolum_average = models.DecimalField(max_digits=12, decimal_places=6, blank=True)
+    jer_id = models.ForeignKey(LkpOblast)
